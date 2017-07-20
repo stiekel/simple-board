5f8f37ac6f2a1aed04eb2dcbc9c606fb20893d0b
stiekel <histkc@gmail.com>
2017-07-21 06:48:10 +0800

    add Spring init

---
+++ src/main/java/com/chensd/simpleboard/SimpleBoardApplication.java
@@ -0,0 +1,12 @@
+package com.chensd.simpleboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleBoardApplication.class, args);
+	}
+}
